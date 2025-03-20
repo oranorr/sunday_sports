7832c347fa21e972d3eb4f1b6235d0b0c7832c88
iaroslav <oliverkarlin0@gmail.com>
2025-03-20 20:30:48 +0700

    Deploy latest changes

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"e672b006cb34c921db85b8e2f482ed3144a457
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "504737382"
+    serviceWorkerVersion: "1735588706"
   }
 });
